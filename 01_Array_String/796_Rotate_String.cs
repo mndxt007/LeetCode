a21c94f9b461c00ca23eb65b6d093a1485ef1b34
mndxt007 <mndxt007@gmail.com>
2026-06-28 22:13:33 +0530

    beats 14%

---
+++ 01_Array_String/796_Rotate_String.cs
@@ -0,0 +1,55 @@
+/*
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+Example 1:
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Example 2:
+Input: s = "abcde", goal = "abced"
+Output: false
+
+Constraints:
+1 <= s.length, goal.length <= 100
+s and goal consist of lowercase English letters.
+*/
+
+// TODO: Implement solution
+bool RotateString(string s, string goal)
+{
+    if(string.Equals(s,goal,StringComparison.Ordinal))
+    {
+        return true;
+    }
+    var goalSpan = goal.AsSpan();
+    var stringSpan = s.AsSpan();
+    for(int i=1; i < s.Length; i++)
+    {
+        bool foundLeft = stringSpan.IndexOf(goalSpan[..i]) > -1;
+        bool foundRight = stringSpan.IndexOf(goalSpan[i..]) > -1;
+        if(foundLeft && foundRight)
+            return true;
+    }
+
+    return false;
+
+}
+
+
+
+List<(string s, string goal)> testcases = [
+    // ("abcde", "cdeab"),
+    // ("abcde", "abced"),
+    // ("w", "w"),
+    ("yn","xi")
+];
+
+foreach (var (s, goal) in testcases)
+{
+    Console.WriteLine($"Testcase: s-{s} goal-{goal}");
+    Console.WriteLine($"RotateString - {RotateString(s, goal)}");
+}
